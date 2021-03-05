86a03b5b8ea38f4cb7e3ad67d6df85b637316639
蒋晨成 <jccdragon@icloud.com>
2021-03-05 18:32:29 +0800

    import

--- Jeak/ViewController.swift
+++ Jeak/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var box = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        self.view.addSubview(box)
+        box.backgroundColor = .blue
+        
+        
     }
 
 
